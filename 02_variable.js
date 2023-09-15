4d3d05d7d2e923d49e803a8a2a69aa80b7d642f8
Shruti kumari <87003332+123soniyo@users.noreply.github.com>
2023-09-15 16:36:37 +0000

    pushing variable file

---
+++ 02_variable.js
@@ -0,0 +1,9 @@
+const empId=11192677
+let empName="shruti"
+var empEmail="shruti@facebook.com" //not use to prefer bcz of issues in block scope and functional scope
+empNo=589763548
+let empAddress;
+
+console.log(empId);
+console.table([empId,empName,empNo,empAddress])
+
